83dae0b8f82a267acdaf146664618e4e48598f20
SHalawi-Dev <SHalawi.dev@google.com>
2021-03-23 02:17:54 +0200

    Changed to funcyional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {
   Card,
   CardImg,
@@ -9,60 +9,62 @@ import {
   ListGroupItem,
 } from 'reactstrap'
 
-export default class Dishdetail extends Component {
-  renderDish(dish) {
-    if (dish != null) {
-      return (
-        <div className='col-12 col-md-5 m-1'>
-          <Card>
-            <CardImg top src={dish.image} alt={dish.name} />
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
-        </div>
-      )
-    } else return <div></div>
-  }
+function RenderDish({ dish }) {
+  if (dish != null) {
+    return (
+      <div className='col-12 col-md-5 m-1'>
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    )
+  } else return <div></div>
+}
 
-  formatDate(d) {
-    const date = new Date(d)
-    console.log(date)
-    return date.toDateString()
-  }
-  renderComments(comments) {
-    if (comments != null)
-      return (
-        <div className='col-12 col-md-5 m-1'>
-          <h4>Comments</h4>
-          <ListGroup as='ul'>
-            {comments.map((comment, index) => {
-              return (
-                <ListGroupItem as='li' key={index}>
-                  <div>{comment.comment}</div>
-                  <div>
-                    -- {comment.author} , {this.formatDate(comment.date)}
-                  </div>
-                </ListGroupItem>
-              )
-            })}
-          </ListGroup>
-        </div>
-      )
-    else return <div></div>
-  }
+function RenderComments({ comments }) {
+  if (comments != null)
+    return (
+      <div className='col-12 col-md-5 m-1'>
+        <h4>Comments</h4>
+        <ListGroup as='ul'>
+          {comments.map((comment, index) => {
+            return (
+              <ListGroupItem as='li' key={index}>
+                <div>{comment.comment}</div>
+                <div>
+                  -- {comment.author} ,{' '}
+                  {new Intl.DateTimeFormat('en-US', {
+                    year: 'numeric',
+                    month: 'short',
+                    day: '2-digit',
+                  }).format(new Date(Date.parse(comment.date)))}
+                </div>
+              </ListGroupItem>
+            )
+          })}
+        </ListGroup>
+      </div>
+    )
+  else return <div></div>
+}
 
-  render() {
-    if (this.props.selectedDish != null) {
-      return (
+const DishDetail = (props) => {
+  if (props.dish != null) {
+    return (
+      <div className='container'>
         <div className='row'>
-          {this.renderDish(this.props.selectedDish)}
-          {this.renderComments(this.props.selectedDish.comments)}
+          <RenderDish dish={props.dish} />
+          <RenderComments comments={props.dish.comments} />
         </div>
-      )
-    } else {
-      return <div></div>
-    }
+      </div>
+    )
+  } else {
+    return <div></div>
   }
 }
+
+export default DishDetail
